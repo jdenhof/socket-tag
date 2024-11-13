163b735bfeb801bafbd68211ee93f1a8b4f541ca
Jagger Denhof <denhofja@mail.gvsu.edu>
2024-11-12 21:05:22 -0500

    added initial game loop/socket connections

---
+++ client.py
@@ -0,0 +1,116 @@
+import threading
+import queue
+import json
+import socket
+import sys
+import argparse
+
+import pygame
+
+from game import *
+
+# Queue to hold player inputs to be sent to the server
+input_queue = queue.Queue()
+position_lock = threading.Lock()
+player_positions = {}
+
+# Initialize Pygame
+def game():
+    print("Starting Game Loop")
+    pygame.init()
+
+    # Screen settings
+    width, height = 400, 400
+    screen = pygame.display.set_mode((width, height))
+
+    pygame.display.set_caption("Tag")
+
+    # Main loop
+    running = True
+    while running:
+        # Fill the background
+        screen.fill(Colors.WHITE)
+        # Event handling
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+        command = None
+        # Get keys pressed
+        keys = pygame.key.get_pressed()
+        if keys.count(True) > 1:
+            print(keys.count(True))
+            pass
+        # Movement based on key presses
+        if keys.count(True) > 1:
+            pass
+        elif keys[pygame.K_w] or keys[pygame.K_UP]:
+            command = { "action": "move", "direction": "up" }
+        elif keys[pygame.K_s] or keys[pygame.K_DOWN]:
+            command = { "action": "move", "direction": "down" }
+        elif keys[pygame.K_a] or keys[pygame.K_LEFT]:
+            command = { "action": "move", "direction": "left" }
+        elif keys[pygame.K_d] or keys[pygame.K_RIGHT]:
+            command = { "action": "move", "direction": "right" }
+
+        if command:
+            command = json.dumps(command)
+            input_queue.put(command)
+        with position_lock:
+            for name, p in player_positions.items():
+                pygame.draw.circle(screen, Colors.BLUE if p["it"] else Colors.RED, (p['x'], p['y']), GameConfig.PLAYER_SIZE)
+
+        # Update the display
+        pygame.display.flip()
+        # Frame rate
+        pygame.time.Clock().tick(30)
+
+# Function to send data to the server continuously
+def send_data(sock):
+    while True:
+
+        if not input_queue.empty():
+            try:
+                command = input_queue.get_nowait()
+            except queue.Empty:
+                continue
+            try:
+                sock.sendall(command.encode())
+            except (BrokenPipeError, OSError) as e:
+                print("Error sending data:", e)
+                break
+
+# Function to receive data from the server continuously
+def receive_data(sock):
+    global player_positions
+    while True:
+        try:
+            data = sock.recv(1024).decode()
+            if data:
+                with position_lock:
+                    data = data.split('\n')[-2]
+                    player_positions = json.loads(data)
+                print("Received position update:", player_positions)
+        except (ConnectionResetError, OSError) as e:
+            print("Error receiving data:", e)
+            break
+
+def client_main(host, port):
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((host, port))
+
+    threading.Thread(target=send_data, args=(sock,), daemon=True).start()
+    threading.Thread(target=receive_data, args=(sock,), daemon=True).start()
+
+    game()
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--host", default='127.0.0.1', type=str)
+    parser.add_argument("--port", default=5555, type=int)
+    args = parser.parse_args()
+
+    client_main(args.host, args.port)
---
+++ server.py
@@ -0,0 +1,122 @@
+# Builtins
+import threading
+import argparse
+import socket
+import json
+import time
+import queue
+# Relative
+from game import *
+
+player_positions = {}
+position_lock = threading.Lock()
+it_lock = threading.Lock()
+clients = []
+input_queue = queue.Queue()
+
+it_player = None
+
+
+def handle_client(conn, addr):
+    global it_player
+    player_id = addr[1]
+
+    with position_lock:
+        if it_player == None:
+            it_player = player_id
+        player_positions[player_id] = {"x": 0, "y": 0, "it": it_player == player_id }
+
+    while True:
+        try:
+            data = conn.recv(1024).decode()
+            if data:
+                input_queue.put((player_id, data))  # Enqueue client input
+                print("Received data from player:", player_id)
+        except (ConnectionResetError, OSError):
+            clients.remove(conn)
+            conn.close()
+            break
+        finally:
+            with position_lock:
+                if player_positions.get(player_id):
+                    print("Removing player", player_id)
+                    del player_positions[player_id]
+            with it_lock:
+                if player_id == it_player:
+                    it_player = None
+
+def broadcast_positions(interval=0.03):
+    while True:
+        start_time = time.time()
+        for conn in clients:
+            try:
+                position_data = json.dumps(player_positions).encode()
+                print("Position data broadcasting:", position_data)
+                position_data += b"\n"
+                conn.sendall(position_data)
+            except (BrokenPipeError, OSError):
+                clients.remove(conn)
+                conn.close()
+        time.sleep(max(0, interval - (time.time() - start_time)))
+
+def game_loop():
+    global player_positions
+    global it_player
+
+    while True:
+        # Process each client input
+        while not input_queue.empty():
+            try:
+                player_id, client_input = input_queue.get_nowait()
+                print("Got input from player: ", player_id, client_input)
+                with position_lock:
+                    if client_input["action"] == "move":
+                        if client_input["direction"] == "up":
+                            player_positions[player_id]["y"] -= GameConfig.MAX_SPEED
+                        elif client_input["direction"] == "down":
+                            player_positions[player_id]["y"] += GameConfig.MAX_SPEED
+                        elif client_input["direction"] == "left":
+                            player_positions[player_id]["x"] -= GameConfig.MAX_SPEED
+                        elif client_input["direction"] == "right":
+                            player_positions[player_id]["x"] += GameConfig.MAX_SPEED
+            except queue.Empty:
+                pass
+
+        with position_lock:
+            for player_id, player in player_positions.items():
+                # Process player input
+                position = player_positions[it_player]
+                if (
+                    abs(player['x'] - position['x']) <=  GameConfig.COLLISION_DIST \
+                        and abs(player['y'] - position['y']) <= GameConfig.COLLISION_DIST
+                ):
+                    with it_lock:
+                        with position_lock:
+                            it_player = player_id
+                            position["it"] = False
+                            player["it"] = True
+        time.sleep(0.03)
+
+def server_main(host, port):
+
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((host, port))
+    server.listen()
+
+    threading.Thread(target=broadcast_positions, daemon=True).start()
+    threading.Thread(target=game_loop, daemon=True).start()
+
+    while True:
+        conn, addr = server.accept()
+        clients.append(conn)
+        threading.Thread(target=handle_client, args=(conn, addr)).start()
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--host", default='127.0.0.1', type=str)
+    parser.add_argument("--port", default=5555, type=int)
+    args = parser.parse_args()
+
+    server_main(args.host, args.port)
