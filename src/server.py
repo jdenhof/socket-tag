827aae1e9f567befe7ddfbd1746d2bf76276609f
Jagger Denhof <denhofja@mail.gvsu.edu>
2024-11-12 22:24:38 -0500

    added tag thread and wait condition for tag delay

--- src/server.py
+++ src/server.py
@@ -8,9 +8,10 @@ import queue
 # Relative
 from game import *
 
-player_positions = {}
+player_configs = {}
 position_lock = threading.Lock()
 game_over = threading.Event()
+can_tag = threading.Event()
 it_lock = threading.Lock()
 clients = []
 input_queue = queue.Queue()
@@ -20,7 +21,7 @@ it_player = None
 
 def handle_client(conn, addr):
     global it_player
-    global player_positions
+    global player_configs
     player_id = addr[1]
     print(f"Handeling player connection {player_id}")
 
@@ -28,7 +29,7 @@ def handle_client(conn, addr):
         if it_player == None:
             print(f"Player {player_id} is the first connection so by default is it")
             it_player = player_id
-        player_positions[player_id] = {"x": 0, "y": 0, "it": it_player == player_id }
+        player_configs[player_id] = {"x": 0, "y": 0, "it": it_player == player_id }
 
     while not game_over.is_set():
         try:
@@ -46,9 +47,9 @@ def handle_client(conn, addr):
             conn.close()
 
             with position_lock:
-                if player_positions.get(player_id):
+                if player_configs.get(player_id):
                     print("Removing player", player_id)
-                    del player_positions[player_id]
+                    del player_configs[player_id]
             with it_lock:
                 if player_id == it_player:
                     it_player = None
@@ -58,7 +59,7 @@ def broadcast_positions(interval=0.03):
         start_time = time.time()
         for conn in clients:
             try:
-                position_data = json.dumps(player_positions).encode()
+                position_data = json.dumps(player_configs).encode()
                 position_data += b"\n"
                 conn.sendall(position_data)
             except (BrokenPipeError, OSError):
@@ -66,33 +67,54 @@ def broadcast_positions(interval=0.03):
                 conn.close()
         time.sleep(max(0, interval - (time.time() - start_time)))
 
-def it_loop():
-    global player_positions
+def player_tagged(player_id):
+    global player_configs
     global it_player
+
+    can_tag.clear()
+
+    old_it_player = it_player
+
+    with it_lock:
+        it_player = player_id
+
+    with position_lock:
+        player_configs[old_it_player]["it"] = False
+        player_configs[player_id]["it"] = True
+        player_configs[player_id]["it_delay"] = True
+
+    time.sleep(GameConfig.TAG_DELAY)
+
+    with position_lock:
+        del player_configs[player_id]["it_delay"]
+
+    can_tag.set()
+
+def it_loop():
+    global player_configs
+    global it_player
+    can_tag.set()
+
     while not game_over.is_set():
-        new_it_player = None
+        can_tag.wait()
         with position_lock:
             with it_lock:
-                it_player_pos = player_positions.get(it_player)
+                it_player_pos = player_configs.get(it_player)
                 if it_player_pos:
-                    for player_id, player in player_positions.items():
+                    for player_id, player in player_configs.items():
                         if player_id == it_player:
                             continue
                         # Process player input
                         x_dist = abs(player['x'] - it_player_pos['x'])
                         y_dist = abs(player['y'] - it_player_pos['y'])
-                        print(f"Distance from {it_player}->{player_id} x: {x_dist} y: {y_dist}")
                         if (x_dist <=  GameConfig.COLLISION_DIST and y_dist <= GameConfig.COLLISION_DIST):
-                            new_it_player = player_id
-                    if new_it_player:
-                        it_player = new_it_player
-                        it_player_pos["it"] = False
-                        player_positions[it_player]["it"] = True
+                            threading.Thread(target=player_tagged, args=(player_id,)).start()
+                            break
 
-        time.sleep(GameConfig.TAG_DELAY if new_it_player else GameConfig.SERVER_SLEEP)
+        time.sleep(GameConfig.SERVER_SLEEP)
 
 def movement_loop():
-    global player_positions
+    global player_configs
     global it_player
 
     while not game_over.is_set():
@@ -100,20 +122,20 @@ def movement_loop():
         while input_queue.not_empty:
             try:
                 player_id, client_input = input_queue.get_nowait()
-                if player_positions.get(player_id) == None:
+                if player_configs.get(player_id) == None:
                     game_over.set()
-                    raise RuntimeError(f"Attempting to modify player position for player {player_id} that doesn't exist\n\t{player_positions}")
+                    raise RuntimeError(f"Attempting to modify player position for player {player_id} that doesn't exist\n\t{player_configs}")
 
                 with position_lock:
                     if client_input["action"] == "move":
                         if client_input["direction"] == "up":
-                            player_positions[player_id]["y"] -= GameConfig.MAX_SPEED
+                            player_configs[player_id]["y"] -= GameConfig.MAX_SPEED
                         elif client_input["direction"] == "down":
-                            player_positions[player_id]["y"] += GameConfig.MAX_SPEED
+                            player_configs[player_id]["y"] += GameConfig.MAX_SPEED
                         elif client_input["direction"] == "left":
-                            player_positions[player_id]["x"] -= GameConfig.MAX_SPEED
+                            player_configs[player_id]["x"] -= GameConfig.MAX_SPEED
                         elif client_input["direction"] == "right":
-                            player_positions[player_id]["x"] += GameConfig.MAX_SPEED
+                            player_configs[player_id]["x"] += GameConfig.MAX_SPEED
             except queue.Empty:
                 pass
 
