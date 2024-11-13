13bb7dfd0f9e7480b18e0ca6513b6f24200c3f4c
Jagger Denhof <denhofja@mail.gvsu.edu>
2024-11-12 20:59:21 -0500

    added game config

---
+++ game.py
@@ -0,0 +1,11 @@
+class GameConfig:
+    MAX_SPEED = 10
+    PLAYER_SIZE = 20
+    TAG_DELAY = 60
+    COLLISION_DIST = 25
+
+class Colors:
+    WHITE = (255, 255, 255)
+    BLUE = (0, 0, 255)
+    YELLOW = (0, 255, 0)
+    RED = (255, 0, 0)
